42e17c3b22543a99ce336ae0fa7df78a92e5a2e9
Chris Botaish <chbota@users.noreply.github.com>
2022-04-03 11:10:30 -0400

    Initial Commit

---
+++ otpfinder/otpfinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  otpfinderApp.swift
+//  otpfinder
+//
+//  Created by Chris Botaish on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct otpfinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
